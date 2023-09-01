a6523ca76766ba6bcbbda852238fbca780f07adf
walaa99t <82266432+walaa99t@users.noreply.github.com>
2023-09-02 02:07:55 +0300

    Add files via upload

---
+++ content.ts
@@ -0,0 +1,26 @@
+import { createStore } from "redux";
+
+import { createChromeStore } from "../popup/redux/chrome-storage";
+import { rootReducer } from "../popup/redux/reducers";
+
+import { DOMWatcher } from "./DOMWatcher/DOMWatcher";
+import { ImageFilter } from "./Filter/ImageFilter";
+
+const init = (): void => {
+  const imageFilter = new ImageFilter();
+  const domWatcher = new DOMWatcher(imageFilter);
+  domWatcher.watch();
+
+  createChromeStore({ createStore })(rootReducer)
+    .then((store) => {
+      const { filterEffect } = store.getState().settings;
+      imageFilter.setSettings({ filterEffect });
+    })
+    .catch((error) => {
+      console.warn(error);
+      imageFilter.setSettings({ filterEffect: "blur" });
+    });
+};
+
+// Ignore iframes, https://stackoverflow.com/a/326076/10432429
+if (window.self === window.top) init();
